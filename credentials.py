1bacfe0b1afc612290a334984ef336edac11699f
Julian Weber <github@juwbr.de>
2023-04-23 20:20:16 +0200

    Moved credentials to json
    Request from #4

--- credentials.py
+++
@@ -1,5 +0,0 @@
-# Credentials for the Spotify API
-# https://developer.spotify.com/dashboard/applications
-
-CLIENT_ID = 'your_client_id'
-CLIENT_SECRET = 'your_client_secret'
--- get_token.py
+++ get_token.py
@@ -1,11 +1,15 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from credentials import CLIENT_ID, CLIENT_SECRET
+import json
+
+credentials = {}
+with open('credentials.json', 'r') as f:
+    credentials = json.load(f)
 
 scope = ['ugc-image-upload', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-private', 'playlist-modify-public', 'user-follow-modify', 'user-follow-read', 'user-read-playback-position', 'user-top-read', 'user-read-recently-played', 'user-library-modify', 'user-library-read', 'user-read-email', 'user-read-private']
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                client_secret=CLIENT_SECRET,                                                          
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=credentials['client_id'],
+                                                client_secret=credentials['client_secret'],                                                          
                                                 redirect_uri="http://localhost",
                                                 scope=scope))
 
